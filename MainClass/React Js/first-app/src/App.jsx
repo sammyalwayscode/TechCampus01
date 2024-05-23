7d6efd6bcbc501667661a82beca4aa5abd605e1b
sammyalwayscode <olorundasamuel2@gmail.com>
2024-05-23 23:27:38 +0100

    Started React Js

---
+++ MainClass/React Js/first-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+// import imageOne from "./assets/mail.jpg";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="LogoHold">
+        <h1>Logo</h1>
+        {/* <img src={imageOne} alt="" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
