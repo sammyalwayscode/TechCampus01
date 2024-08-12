22a1d96a250f8e80268841439ae3b6111da320e9
sammyalwayscode <olorundasamuel2@gmail.com>
2024-08-12 15:59:28 +0100

    New Changes

--- MainClass/React Js/SAIL/first-class/src/pages/About.jsx
+++ MainClass/React Js/SAIL/first-class/src/pages/About.jsx
@@ -1,7 +1,26 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const About = () => {
-  return <Container>About</Container>;
+  const [num, setNum] = useState(0);
+  console.log(num);
+
+  const addNum = () => {
+    setNum(num + 5);
+  };
+
+  const subtractNum = () => {
+    setNum(num - 5);
+  };
+
+  return (
+    <Container>
+      <h2>Number Increase</h2>
+      <h1> {num} </h1>
+      <button onClick={addNum}>Add</button>
+      <button onClick={subtractNum}>Minus</button>
+    </Container>
+  );
 };
 
 export default About;
@@ -9,4 +28,12 @@ export default About;
 const Container = styled.div`
   height: 100vh;
   background-color: darkmagenta;
+  padding: 20px;
+  h2 {
+    color: cyan;
+  }
+  h1 {
+    font-size: 50px;
+    color: #fff;
+  }
 `;
--- MainClass/React Js/SAIL/first-class/src/pages/Contact.jsx
+++ MainClass/React Js/SAIL/first-class/src/pages/Contact.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Contact = () => {
-  return <Container>Contact</Container>;
+  const [name, setName] = useState("");
+  const [toggle, setToggle] = useState(false);
+  console.log(name);
+
+  const handleToogle = () => {
+    setToggle(!toggle);
+  };
+
+  console.log(toggle);
+
+  return (
+    <Container>
+      <h2>Form Fild</h2>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <br />
+      <input type="email" placeholder="Email" />
+      <br />
+      <input type="text" placeholder="Address" />
+      <br />
+      <button onClick={handleToogle}>Submmit</button>
+
+      {toggle ? <h1> Welcome {name} </h1> : null}
+    </Container>
+  );
 };
 
 export default Contact;
@@ -9,4 +40,6 @@ export default Contact;
 const Container = styled.div`
   height: 100vh;
   background-color: darkcyan;
+  color: #fff;
+  padding: 30px;
 `;
